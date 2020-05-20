0524b1106405d9cfac62006fba02e117d47ffb05
SaraCremonesi <saracrem00@gmail.com>
2020-05-20 16:31:11 +0200

    completamento esercizio

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ var passengerAge = parseInt(prompt('How old are you?'));
 
 // Full price ticket
 var fullPriceTicket = numberOfKm * 0.21;
+var juniorPriceTicket;
+var seniorPriceTicket;
 
 if (passengerAge < 18) {
   juniorPriceTicket = (fullPriceTicket * 20) / 100;
-  document.getElementById('ticket-price').innerHTML = juniorPriceTicket;
+  document.getElementById('ticket-price').innerHTML = juniorPriceTicket.toFixed(2);
 } else if (passengerAge > 65) {
   seniorPriceTicket = (fullPriceTicket * 40) / 100;
-  document.getElementById('ticket-price').innerHTML = seniorPriceTicket;
+  document.getElementById('ticket-price').innerHTML = seniorPriceTicket.toFixed(2);
 } else {
-  document.getElementById('ticket-price').innerHTML = fullPriceTicket;
+  document.getElementById('ticket-price').innerHTML = fullPriceTicket.toFixed(2);
 }
